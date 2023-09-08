dc37599b3d77cea09acd7e047c289f6558e3d24a
Héctor Cabrera <me@cabrerahector.com>
2023-09-08 16:28:16 -0400

    Fixes spaces for readability

--- feed.php
+++ feed.php
@@ -6,14 +6,14 @@
  */
 
 // File is being accessed directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if ( ! defined('WPINC') ) {
     die;
 }
 
 header('Content-Type: ' . feed_content_type('rss2') . '; charset=' . get_option('blog_charset'), true);
 $more = 1;
 
-echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
+echo '<?xml version="1.0" encoding="' . get_option('blog_charset') . '"?' . '>';
 
 /**
  * Fires between the xml and rss tags in a feed.
@@ -23,7 +23,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>';
  * @param string $context Type of feed. Possible values include 'rss2', 'rss2-comments',
  *                        'rdf', 'atom', and 'atom-comments'.
  */
-do_action( 'rss_tag_pre', 'rss2' );
+do_action('rss_tag_pre', 'rss2');
 ?>
 <rss version="2.0"
     xmlns:content="http://purl.org/rss/1.0/modules/content/"
@@ -38,7 +38,7 @@ do_action( 'rss_tag_pre', 'rss2' );
      *
      * @since 2.0.0
      */
-    do_action( 'rss2_ns' );
+    do_action('rss2_ns');
     ?>
 >
 
@@ -48,10 +48,10 @@ do_action( 'rss_tag_pre', 'rss2' );
     <link><?php bloginfo_rss('url') ?></link>
     <description><?php bloginfo_rss("description") ?></description>
     <lastBuildDate><?php
-        $date = get_lastpostmodified( 'GMT' );
-        echo $date ? mysql2date( 'r', $date, false ) : date( 'r' );
+        $date = get_lastpostmodified('GMT');
+        echo $date ? mysql2date('r', $date, false) : date('r');
     ?></lastBuildDate>
-    <language><?php bloginfo_rss( 'language' ); ?></language>
+    <language><?php bloginfo_rss('language'); ?></language>
     <sy:updatePeriod><?php
         $duration = 'hourly';
 
@@ -63,7 +63,7 @@ do_action( 'rss_tag_pre', 'rss2' );
          * @param string $duration The update period. Accepts 'hourly', 'daily', 'weekly', 'monthly',
          *                         'yearly'. Default 'hourly'.
          */
-        echo apply_filters( 'rss_update_period', $duration );
+        echo apply_filters('rss_update_period', $duration);
     ?></sy:updatePeriod>
     <sy:updateFrequency><?php
         $frequency = '1';
@@ -76,7 +76,7 @@ do_action( 'rss_tag_pre', 'rss2' );
          * @param string $frequency An integer passed as a string representing the frequency
          *                          of RSS updates within the update period. Default '1'.
          */
-        echo apply_filters( 'rss_update_frequency', $frequency );
+        echo apply_filters('rss_update_frequency', $frequency);
     ?></sy:updateFrequency>
     <?php
     /**
@@ -84,69 +84,76 @@ do_action( 'rss_tag_pre', 'rss2' );
      *
      * @since 2.0.0
      */
-    do_action( 'rss2_head');
+    do_action('rss2_head');
 
     $args = array(
         'range' => 'last7days',
-        'limit' => get_option( 'posts_per_rss' )
+        'limit' => get_option('posts_per_rss')
     );
-    $args = apply_filters( 'popular_posts_feed_args', $args );
-    $popular_posts = new \WordPressPopularPosts\Query( $args );
+    $args = apply_filters('popular_posts_feed_args', $args);
+    $popular_posts = new \WordPressPopularPosts\Query($args);
 
     if ( $popular_posts->get_posts() ) :
         foreach( $popular_posts->get_posts() as $popular_post ) :
-    ?>
-    <item>
-        <title><?php echo apply_filters( 'the_title_rss', $popular_post->title ); ?></title>
-        <link><?php echo esc_url( apply_filters( 'the_permalink_rss', get_permalink($popular_post->id) ) ); ?></link>
-<?php if ( get_comments_number( $popular_post->id ) || comments_open( $popular_post->id ) ) : ?>
-        <comments><?php echo esc_url( apply_filters( 'comments_link_feed', get_comments_link($popular_post->id) ) ); ?></comments>
-<?php endif; ?>
-        <pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true, $popular_post->id), false); ?></pubDate>
-        <dc:creator><![CDATA[<?php echo get_the_author_meta( 'display_name', $popular_post->uid ); ?>]]></dc:creator>
-        <?php
-        $categories = get_the_category( $popular_post->id );
-        $cat_names = array();
+            ?>
+            <item>
+                <title><?php echo apply_filters('the_title_rss', $popular_post->title); ?></title>
+                <link><?php echo esc_url( apply_filters('the_permalink_rss', get_permalink($popular_post->id)) ); ?></link>
+                <?php if ( get_comments_number($popular_post->id) || comments_open($popular_post->id) ) : ?>
+                    <comments><?php echo esc_url( apply_filters('comments_link_feed', get_comments_link($popular_post->id)) ); ?></comments>
+                <?php endif; ?>
+                <pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true, $popular_post->id), false); ?></pubDate>
+                <dc:creator><![CDATA[<?php echo get_the_author_meta('display_name', $popular_post->uid); ?>]]></dc:creator>
+                <?php
+                $categories = get_the_category($popular_post->id);
+                $cat_names = array();
 
-        if ( !empty($categories) ) foreach ( (array) $categories as $category ) {
-            $cat_names[] = sanitize_term_field( 'name', $category->name, $category->term_id, 'category', 'rss' );
-        }
+                if ( !empty($categories) ) foreach ( (array) $categories as $category ) {
+                    $cat_names[] = sanitize_term_field('name', $category->name, $category->term_id, 'category', 'rss');
+                }
 
-        foreach ( $cat_names as $cat_name ) {
-            echo "\t\t<category><![CDATA[" . @html_entity_decode( $cat_name, ENT_COMPAT, get_option('blog_charset') ) . "]]></category>\n";
-        }
-        ?>
+                foreach ( $cat_names as $cat_name ) {
+                    echo "\t\t<category><![CDATA[" . @html_entity_decode($cat_name, ENT_COMPAT, get_option('blog_charset')) . "]]></category>\n";
+                }
+                ?>
+                <guid isPermaLink="false"><?php the_guid($popular_post->id); ?></guid>
+                <?php $post = get_post($popular_post->id ); setup_postdata($post); ?>
+                <description><![CDATA[<?php echo apply_filters('the_excerpt_rss', get_the_excerpt($popular_post->id)); ?>]]></description>
+                <?php
+                if ( ! get_option('rss_use_excerpt') ) :
+                    $content = apply_filters('the_content', $post->post_content);
+                    $content = str_replace(']]>', ']]&gt;', $content);
+                    $content = apply_filters('the_content_feed', $content, 'rss2');
 
-        <guid isPermaLink="false"><?php the_guid( $popular_post->id ); ?></guid>
-<?php $post = get_post( $popular_post->id ); setup_postdata( $post ); ?>
-        <description><![CDATA[<?php echo apply_filters( 'the_excerpt_rss', get_the_excerpt( $popular_post->id ) ); ?>]]></description>
-<?php if ( ! get_option('rss_use_excerpt') ) : ?>
-    <?php
-    $content = apply_filters( 'the_content', $post->post_content );
-    $content = str_replace(']]>', ']]&gt;', $content);
-    $content = apply_filters( 'the_content_feed', $content, 'rss2' );
-    ?>
-    <?php if ( strlen( $content ) > 0 ) : ?>
-        <content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
-    <?php else : ?>
-        <content:encoded><![CDATA[<?php echo apply_filters( 'the_excerpt_rss', get_the_excerpt( $popular_post->id ) ); ?>]]></content:encoded>
-    <?php endif; ?>
-<?php endif; ?>
-<?php if ( get_comments_number( $popular_post->id ) || comments_open( $popular_post->id ) ) : ?>
-        <wfw:commentRss><?php echo esc_url( get_post_comments_feed_link( $popular_post->id, 'rss2' ) ); ?></wfw:commentRss>
-        <slash:comments><?php echo get_comments_number( $popular_post->id ); ?></slash:comments>
-<?php endif; ?>
-<?php rss_enclosure(); ?>
-    <?php
-    /**
-     * Fires at the end of each RSS2 feed item.
-     *
-     * @since 2.0.0
-     */
-    do_action( 'rss2_item' );
-    ?>
-    </item>
-    <?php
+                    if ( strlen( $content ) > 0 ) :
+                        ?>
+                        <content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
+                        <?php
+                    else :
+                        ?>
+                        <content:encoded><![CDATA[<?php echo apply_filters('the_excerpt_rss', get_the_excerpt($popular_post->id)); ?>]]></content:encoded>
+                        <?php
+                    endif;
+                endif;
+
+                if ( get_comments_number($popular_post->id) || comments_open($popular_post->id) ) :
+                    ?>
+                    <wfw:commentRss><?php echo esc_url( get_post_comments_feed_link($popular_post->id, 'rss2') ); ?></wfw:commentRss>
+                    <slash:comments><?php echo get_comments_number($popular_post->id); ?></slash:comments>
+                    <?php
+                endif;
+
+                rss_enclosure();
+
+                /**
+                 * Fires at the end of each RSS2 feed item.
+                 *
+                 * @since 2.0.0
+                 */
+                do_action('rss2_item');
+                ?>
+            </item>
+            <?php
         endforeach;
     endif;
     ?>
